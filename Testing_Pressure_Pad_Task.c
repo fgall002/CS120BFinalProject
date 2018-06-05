6aa17bd370768f89861b62bd1c00f67c0b3a39b7
Francisco Gallego <fgall002@ucr.edu>
2018-06-04 23:46:35 -0700

    Second Test

--- Testing_Pressure_Pad_Task.c
+++ Testing_Pressure_Pad_Task.c
@@ -1,8 +1,9 @@
 #include <avr/io.h>
 #include "io.c"
+#include "timer.h"
 
 
-#define A3 ~PINA & 0x04
+#define A3 (~PINA & 0x08)
 
 //global variables
 unsigned char Pad;
@@ -10,60 +11,65 @@ unsigned char Pad;
 
 //Testing CODE
 unsigned char game_on;
+unsigned char state;
 
 //Enum
 enum Pad_State{Pad_Start, Wait_Pressure, Down, Hold_Pressure}Pad_State;
 
 void Pad_Tick(){
 
-  //Transition
+	//Transition
 
-  switch(Pad_State){
-    case Pad_Start:
-        Pad = 0;
-        //Testing code
-        game_on = 1;
-        //
-        Pad_State = Wait_Pressure;
-      break;
-    case Wait_Pressure:
-        if(!(game_on) || !(A3)){
-          Pad_State= Wait_Pressure;
-        }
-        else if(game_on && A3){
-          Pad_State = Down;
-        }
-      break;
-    case Down:
-        Pad = 0;
-        Pad_State =Hold_Pressure;
-      break;
-    case Hold_Pressure:
-      if(A3){
-        Pad_State = Hold_Pressure;
-      }
-      else if(!(A3)){
-        Pad_State = Wait_Pressure
-      }
-      break;
-    default:
-      break;
-  }
+	switch(Pad_State){
+		case Pad_Start:
+		Pad = 0;
+		//Testing code
+		game_on = 1;
+		state =1;
+		//
+		Pad_State = Wait_Pressure;
+		break;
+		case Wait_Pressure:
+		state =2;
+		if(!(game_on) || !(A3)){
+			Pad_State= Wait_Pressure;
+		}
+		else if(game_on && A3){
+			Pad_State = Down;
+		}
+		break;
+		case Down:
+		state =3;
+		Pad = 0;
+		Pad_State =Hold_Pressure;
+		break;
+		case Hold_Pressure:
+		state =4 ;
+		if(A3){
+			Pad_State = Hold_Pressure;
+		}
+		else if(!(A3)){
+			Pad_State = Wait_Pressure;
+		}
+		break;
+		default:
+		break;
+	}
 
-  //Actions
-  switch(Pad_State){
-    case Pad_Start:
-      break;
-    case Wait_Pressure:
-      break;
-    case Down:
-        Pad = 1;
-      break;
-    case Hold_Pressure:
-      break;
-    default:
-      break;
-  }
+	//Actions
+	switch(Pad_State){
+		case Pad_Start:
+		break;
+		case Wait_Pressure:
+		break;
+		case Down:
+		Pad = 1;
+		break;
+		case Hold_Pressure:
+		break;
+		default:
+		break;
+	}
 }
 
 
@@ -76,14 +82,15 @@ int main(void){
 	LCD_init();
 	TimerSet(100);
 	TimerOn();
-	Timer_CTRL_State = Timer_Start;
+	Pad_State = Pad_Start;
 
 	while(1) {
 		//Start_Button_Handler_Tick();
 		//Pad_Tick();
-		Timer_CTRL_Tick();
+		//Timer_CTRL_Tick();
+		Pad_Tick();
 		LCD_Cursor(1);
-		LCD_WriteData(pad + '0');
+		LCD_WriteData(state + '0');
 
 		while (!TimerFlag);	// Wait 1 sec
 		TimerFlag = 0;
